5c8188876b7733b90456f16747d2b6934262385c
mtchoum1 <mtchoum1@bu.edu>
2024-10-04 20:05:06 -0400

    HW3

---
+++ HW3/wildlife_tracker/migration_tracking/migration_path.py
@@ -0,0 +1,15 @@
+from typing import Optional
+
+from wildlife_tracker.habitat_management.habitat import Habitat
+
+class MigrationPath:
+
+    def __init__(self, path_id: int, size: int, species: str, start_date: str, start_location: Habitat, status: str = "Scheduled") -> None:
+
+        pass
+
+    def update_migration_path_details(path_id: int, **kwargs) -> None:
+        pass
+
+    def get_migration_path_details(path_id) -> dict:
+        pass
