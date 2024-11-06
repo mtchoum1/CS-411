4fc7267addd0c7e15a2172254aa166d361cde5d6
andeliuliu <andelerliutv@gmail.com>
2024-11-06 00:05:32 -0500

    added random util testing and docstrings

--- HW4/meal_max/meal_max/utils/random_utils.py
+++ HW4/meal_max/meal_max/utils/random_utils.py
@@ -8,6 +8,16 @@ configure_logger(logger)
 
 
 def get_random() -> float:
+    """
+    Fetches a random decimal number with two decimal places from random.org.
+
+    Returns:
+        float: The random decimal number fetched from random.org.
+
+    Raises:
+        RuntimeError: If the request to random.org fails or times out.
+        ValueError: If the response from random.org is not a valid float.
+    """
     url = "https://www.random.org/decimal-fractions/?num=1&dec=2&col=1&format=plain&rnd=new"
 
     try:
